631e4423c1e7622c6ac0696b95deb0d52e3aea24
Atharva <atharvapise19@gmail.com>
2023-11-16 11:15:13 +0530

    styles: add tailwind dependency and new fonts for text and code content

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,29 +1,38 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://convelations.vercel.app/',
-	integrations: [
-		starlight({
-			title: 'Convelations',
-			social: {
-				github: 'https://github.com/at-the-vr',
-				'x.com': 'https://x.com/at_the_vr',
-			},
-			sidebar: [
-				{
-					label: 'Discord',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Key Points', link: '/discord/key-points/' },
-					],
-				},
-				{
-					label: 'GitHub',
-					autogenerate: { directory: 'github' },
-				},
+  site: 'https://convelations.vercel.app/',
+  integrations: [starlight({
+    title: 'Convelations',
+		customCss: [
+				// Path to your Tailwind base styles:
+				'./src/tailwind.css',
 			],
-		}),
-	],
+
+    social: {
+      github: 'https://github.com/at-the-vr',
+      'x.com': 'https://x.com/at_the_vr'
+    },
+    sidebar: [{
+      label: 'Discord',
+      items: [
+      // Each item here is one entry in the navigation menu.
+      {
+        label: 'Key Points',
+        link: '/discord/key-points/'
+      }]
+    }, {
+      label: 'GitHub',
+      autogenerate: {
+        directory: 'github'
+      }
+    }]
+  }), tailwind({
+		// Disable the default base styles:
+			applyBaseStyles: false,
+	})]
 });
