dff49249336e8aadad225acf0439a5e61f40f8fb
Atharva Pise <atharvapise19@gmail.com>
2023-12-04 12:15:55 +0530

    🔥remove badge for first-steps

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,64 +5,63 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://convelations.vercel.app/',
-	integrations: [
-		starlight({
-			title: 'Convelations',
-			head: [
-				{
-					tag: 'link',
-					attrs: {
-						href: 'https://fonts.googleapis.com',
-						rel: 'preconnect',
-					},
-					tag: 'link',
-					attrs: {
-						href: 'https://fonts.gstatic.com',
-						rel: 'preconnect',
-						crossorigin: true,
-					},
-					tag: 'link',
-					attrs: {
-						href: 'https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap',
-						rel: 'stylesheet',
-					},
-				},
-			],
-			customCss: [
-				// Path to your Tailwind base styles:
-				'./src/tailwind.css',
-			],
-			social: {
-				'x.com': 'https://x.com/at_the_vr',
-			},
-			sidebar: [
-				{
-					label: 'Discord',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{
-							label: 'Key Points around Discord',
-							link: '/discord/key-points/',
-						},
-						{
-							label: 'First Steps',
-							link: '/discord/first-steps/',
-							badge: { text: 'New', variant: 'tip' },
-						},
-					],
-				},
-				{
-					label: 'GitHub',
-					autogenerate: {
-						directory: 'github',
-					},
-				},
-			],
-		}),
-		tailwind({
-			// Disable the default base styles:
-			applyBaseStyles: false,
-		}),
-	],
+  site: 'https://convelations.vercel.app/',
+  integrations: [
+    starlight({
+      title: 'Convelations',
+      head: [
+        {
+          tag: 'link',
+          attrs: {
+            href: 'https://fonts.googleapis.com',
+            rel: 'preconnect',
+          },
+          tag: 'link',
+          attrs: {
+            href: 'https://fonts.gstatic.com',
+            rel: 'preconnect',
+            crossorigin: true,
+          },
+          tag: 'link',
+          attrs: {
+            href: 'https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap',
+            rel: 'stylesheet',
+          },
+        },
+      ],
+      customCss: [
+        // Path to your Tailwind base styles:
+        './src/tailwind.css',
+      ],
+      social: {
+        'x.com': 'https://x.com/at_the_vr',
+      },
+      sidebar: [
+        {
+          label: 'Discord',
+          items: [
+            // Each item here is one entry in the navigation menu.
+            {
+              label: 'Key Points around Discord',
+              link: '/discord/key-points/',
+            },
+            {
+              label: 'First Steps',
+              link: '/discord/first-steps/',
+            },
+          ],
+        },
+        {
+          label: 'GitHub',
+          autogenerate: {
+            directory: 'github',
+          },
+        },
+      ],
+    }),
+    tailwind({
+      // Disable the default base styles:
+      applyBaseStyles: false,
+    }),
+  ],
 });
